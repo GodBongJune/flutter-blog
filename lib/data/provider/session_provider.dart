d549d52b9e3ebb51cf87a48ece4d640c3ae94cc0
bong <darksen7693@gmail.com>
2023-10-18 11:45:42 +0900

    로그아웃 기능 완료

--- lib/data/provider/session_provider.dart
+++ lib/data/provider/session_provider.dart
@@ -57,7 +57,16 @@ class SessionUser {
     }
   }
 
-  Future<void> logout() async {}
+  // jwt는 로그아웃할 때 서버측으로 요청할 필요가 없음.
+  Future<void> logout() async {
+    this.jwt = null;
+    this.isLogin = false;
+    this.user = null;
+
+    await secureStorage.delete(key: "jwt");
+
+    Navigator.pushNamedAndRemoveUntil(mContext!, "/login", (route) => false);
+  }
 }
 
 // 2. 창고 (view model이 아니라 필요없음)
